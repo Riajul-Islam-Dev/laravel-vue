007f76645709700b07491871da74cf475bd9387f
Riajul Islam <ritewu2014@gmail.com>
2023-12-31 13:40:01 +0600

    routes added.

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('posts', 'PostController@index');
+Route::group(['prefix' => 'post'], function () {
+    Route::post('add', 'PostController@add');
+    Route::get('edit/{id}', 'PostController@edit');
+    Route::post('update/{id}', 'PostController@update');
+    Route::delete('delete/{id}', 'PostController@delete');
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
